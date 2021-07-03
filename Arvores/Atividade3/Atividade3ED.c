bccf65a6b99e2290b36bc69886ca2faac6e9bbaf
brunocosta1 <brunocosta.cg1998@gmail.com>
2021-07-03 01:39:17 -0300

    Implementando as funções LerArvore(), as de impressão de uma árvore e as que vão auxiliar no resto das outras funções

---
+++ Arvores/Atividade3/Atividade3ED.c
@@ -0,0 +1,166 @@
+// Alunos: Bruno Costa dos Passos, Kamile Novaes, Raphael Kazuhiro Yoshiki
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+
+typedef struct arvore{
+
+    int info;
+    struct arvore *esq;
+    struct arvore *dir;
+
+}arvore;
+
+
+// Código para verificar qual sistema operacional está rodando.
+
+#ifdef __unix__
+    #include <unistd.h>
+
+#elif defined(_WIN32) || defined(WIN32)
+    #define OS_Windows
+    #include <windows.h>
+#endif
+
+// Função para deixar o menu mais dinâmico.
+
+void limpa_tela(){
+
+    #ifdef OS_Windows
+        system("timeout 1");
+        system("cls");
+    #else
+        system("sleep 1");
+        system("clear");
+    #endif
+
+}
+
+//Função para criar um elemento da árvore
+
+arvore *CriaElemento(int info){
+
+    arvore *nova = (arvore*)malloc(sizeof(arvore));
+    nova->info = info;
+    nova->dir = NULL;
+    nova->esq= NULL;
+
+    return nova;
+
+}
+
+// Função para ler árvore a partir de um arquivo
+
+arvore *LerArvore(arvore *a, FILE *arq){
+
+    char c;
+    int num;
+
+    fscanf(arq, "%c", &c);
+    fscanf(arq, "%d", &num);
+
+    if(num == -1){
+        fscanf(arq, "%c", &c);
+        return NULL;
+    }else{
+
+        arvore *a = CriaElemento(num);
+        a->esq = LerArvore(a,arq);
+        a->dir = LerArvore(a,arq);
+        fscanf(arq, "%c", &c);
+        return a;
+    }
+}
+
+//Retorna a altura de uma árvore ou sub-árvore
+
+int Altura(arvore *a){
+
+	if(a==NULL){
+		return 0;
+	}
+	else{
+		int he, hd;
+		he=Altura(a->esq);
+		hd=Altura(a->dir);
+
+		if(he>hd){
+			return he + 1;
+		}
+		else{
+			return hd + 1;
+		}
+	}
+}
+
+//Imprime os elementos de um determinado nivel
+
+void ImprimirNivel(arvore *a, int cont, int nivel){
+
+	if(a!=NULL){
+		if(cont==nivel){
+			printf("%d ", a->info);
+		}
+		else{
+			ImprimirNivel(a->esq,cont+1,nivel);
+			ImprimirNivel(a->dir,cont+1,nivel);
+		}
+	}
+}
+// Funções de impressão da árvore
+
+void ImprimePreOrdem(arvore *a){
+
+    if(a != NULL){
+        printf("%d ", a->info);
+        ImprimePreOrdem(a->esq);
+        ImprimePreOrdem(a->dir);
+    }
+
+}
+
+void ImprimeEmOrdem(arvore *a){
+
+    if(a != NULL){
+        ImprimeEmOrdem(a->esq);
+        printf("%d ", a->info);
+        ImprimeEmOrdem(a->dir);
+    }
+}
+
+void ImprimePosOrdem(arvore *a){
+
+    if (a != NULL){
+        ImprimePosOrdem(a->esq);
+        ImprimePosOrdem(a->dir);
+        printf("%d ", a->info);
+    }
+
+}
+
+
+void ImprimirLargura(arvore *a){
+
+	int i;
+	int nivel=0;
+
+	if(a!=NULL){
+		for(i=1;i<=Altura(a);i++){
+			ImprimirNivel(a,0,nivel++);
+			printf("\n");
+		}
+	}
+}
+
+
+
+
+
+int main(){
+    
+
+    return 0;
+    
+}
