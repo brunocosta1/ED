8efca6b00e775480d43757143dee9740ace7c079
brunocosta1 <brunocosta.cg1998@gmail.com>
2021-07-20 11:11:20 -0300

    Atualizando o programa

--- Hashing/Atividade6ED_Hashing.c
+++ Hashing/Atividade6ED_Hashing.c
@@ -6,6 +6,29 @@
 
 #define TAM 53 //Turma de 50 alunos, mas colocando o valor do tamanho em 53 para ficar com número primo
 
+// Código para verificar qual sistema operacional está rodando.
+
+#ifdef __unix__
+    #include <unistd.h>
+
+#elif defined(_WIN32) || defined(WIN32)
+    #define OS_Windows
+    #include <windows.h>
+#endif
+
+// Função para deixar o menu mais dinâmico.
+
+void limpa_tela(){
+
+    #ifdef OS_Windows
+        system("timeout 1");
+        system("cls");
+    #else
+        system("sleep 1");
+        system("clear");
+    #endif
+
+}
 typedef struct registro{
 
     int mat;
@@ -18,15 +41,88 @@ typedef struct registro{
 int hash(int key);
 void inicializa(char *nome_arq);
 void inserir(char *nome_arq, registro aluno);
+void imprimir(char *nome_arq, int mat);
 int acha_pos(char *nome_arq, int mat);
-void menu();
+void menu(char *nome_arq);
 
 int main(){
+
+    char nome_arq[10];
+    printf("Digite o nome do arquivo que deseja inicializar: ");
+    scanf("%s", nome_arq);
+    scanf("");
+
+    inicializa(nome_arq);
+    limpa_tela();
+
+    menu(nome_arq);
 
     return 0;
 
 }
 
+void menu(char *nome_arq){
+
+    int opc = 0;
+    int mat_temp;
+    registro aluno_temp;
+
+
+    while(opc != 4){
+
+        printf("[0] - Inserir novo aluno\n");
+        printf("[1] - Imprimir informações de um aluno\n");
+        printf("[2] - Imprimir a tabela de dispersão\n");
+        printf("[3] - Sair\n\n");
+
+        printf("Opção: ");
+        scanf("%d", &opc);
+        printf("\n");
+
+        switch(opc){
+            
+            case 0:
+                printf("Digite o nome do aluno: ");
+                scanf("%s", aluno_temp.nome);
+                printf("\n");
+
+                printf("Digite o nome do curso do aluno: ");
+                scanf("%s", aluno_temp.curso);
+                printf("\n");
+
+                printf("Digite o a matrícula do aluno: ");
+                scanf("%d", &aluno_temp.mat);
+                printf("\n");
+
+                inserir(nome_arq, aluno_temp);
+                limpa_tela();
+
+                break;
+
+            case 1:
+                printf("Digite a matrícula: ");
+                scanf("%d", &mat_temp);
+                printf("\n");
+
+                imprimir(nome_arq, mat_temp);
+
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            default:
+                break;
+        }
+
+
+
+    }
+
+}
 int hash(int key){
 
     return key % TAM;
@@ -42,6 +138,25 @@ void inicializa(char *nome_arq){
 
     for(int i = 0; i < TAM; i++)
         fwrite(&a, sizeof(registro), 1, arq);
+
+    fclose(arq);
+
+}
+void imprimir(char *nome_arq, int mat){
+
+    int pos = acha_pos(nome_arq, mat);
+
+    FILE *arq = fopen(nome_arq, "rb");
+
+    registro a;
+
+    fseek(arq, pos*sizeof(registro), SEEK_SET);
+    fread(&a, sizeof(registro), 1, arq);
+
+
+    printf("Nome: %s\n", a.nome);
+    printf("Curso: %s\n", a.curso);
+    printf("Matrícula: %d\n", a.mat);
 
     fclose(arq);
 
@@ -92,29 +207,5 @@ int acha_pos(char *nome_arq, int mat){
 
 }
 
-void menu(){
 
-    int opc = 0;
-
-    while(opc != 4){
-
-        printf("[0] - Inserir novo aluno\n");
-        printf("[1] - Imprimir informações de um aluno\n");
-        printf("[2] - Imprimir a tabela de dispersão\n");
-        printf("[3] - Sair\n\n");
-
-        printf("Opção: ");
-        scanf("%d", &opc);
-        printf("\n");
-
-        switch(opc){
-            
-
-        }
-
-
-
-    }
-
-}
 
