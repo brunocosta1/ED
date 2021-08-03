73257b5d366b70aee0e173c4314335ee06052ef9
Raphael Kazuhiro Yoshiki <74300967+RaphaelYoshiki@users.noreply.github.com>
2021-08-03 12:07:21 -0300

    Adicionadas funções de contar grau de entrada e saída de um vértice

--- Grafos/GrafosED_Atividade8.c
+++ Grafos/GrafosED_Atividade8.c
@@ -25,15 +25,15 @@ void limpa_tela(){
 }
 
 typedef struct lista{
-	
+
 	int destino;
 	int custo;
 	struct lista *prox;
-	
+
 }lista;
 
 void inicializar(lista **g, int n){
-	
+
 	int i;
 	for(i=0;i<=n;i++){
 		g[i]=NULL;
@@ -41,7 +41,7 @@ void inicializar(lista **g, int n){
 }
 
 lista *InserirLista(lista *l, int d, int c){
-	
+
 	lista *no =(lista*)malloc(sizeof(lista));
 	no->destino=d;
 	no->custo=c;
@@ -50,12 +50,12 @@ lista *InserirLista(lista *l, int d, int c){
 }
 
 void InserirAresta(lista**g, int origem, int destino, int custo){
-	
+
 	g[origem]=InserirLista(g[origem],destino,custo);
 }
 
 lista *RemoverLista(lista *l, int destino, int custo){
-    
+
 	lista *aux;
 
     if(l!=NULL){
@@ -75,54 +75,84 @@ lista *RemoverLista(lista *l, int destino, int custo){
 }
 
 void RemoverAresta(lista **g, int origem, int destino, int custo){
-	
+
     g[origem]=RemoverLista(g[origem],destino,custo);
 }
 
 void ImprimirLista(lista *l){
-	
+
 	if(l!=NULL){
-		
+
 		printf("- (%d,%d) ", l->destino, l->custo);
 		ImprimirLista(l->prox);
 	}
 }
 
-void ImprimirGrafo(lista **g,int n){
-	
+void ImprimirGrafo(lista **g, int n){
+
 	int i;
 	printf("\nGrafo: ");
 	for(i=1;i<=n;i++){
-		
+
 		printf("\n\t %d: ", i);
 		ImprimirLista(g[i]);
 	}
 }
 
+
+int contEntrada(lista *l, int x, int cont) {
+  if (l != NULL) {
+    if (l->destino == x) {
+      cont++;
+    }
+    cont = contEntrada(l->prox, x, cont);
+  }
+  return cont;
+}
+
+void entradaVertice(lista **g, int x, int n) {
+  int ct = 0;
+  for (size_t i = 1; i <= n; i++) {
+    ct = ct + contEntrada(g[i], x, 0);
+  }
+  printf("Grau de entrada do vertice: %d\n", ct);
+}
+
+void saidaVertice(lista *l, int cont) {
+  if (l != NULL) {
+    cont++;
+    saidaVertice(l->prox, cont);
+  }
+  else {
+  printf("Grau de saida do vertice: %d\n", cont);
+  }
+}
+
+
 void menu(lista **g){
-	
+
 	int n=0, x=0;
 	int origem=0, destino=0, arestas=0, custo=0;
 	printf("Qual o tamanho do vetor: ");
 	scanf("%d",&n);
-	
+
 	g=(lista**)malloc((n+1)*sizeof(lista*));
 	inicializar(g,n);
-	
+
 	while(x!=6){
-		
+
 		printf("\n[1] - Inserir uma aresta.\n");
 		printf("[2] - Remover uma aresta.\n");
 		printf("[3] - Imprimir grafo.\n");
 		printf("[4] - Imprimir os graus de entrada e saida de um vertice.\n");
 		printf("[5] - Verificar se um grafo eh completo.\n");
 		printf("[6] - Sair.\n");
-		
+
 		printf("\nDigite a opcao: ");
 		scanf("%d", &x);
-		
+
 		switch(x){
-			
+
 			case 1:
 				printf("\nOrigem: ");
 				scanf("%d", &origem);
@@ -134,7 +164,7 @@ void menu(lista **g){
 				arestas++;
 				limpa_tela();
 				break;
-			
+
 			case 2:
 				printf("\nOrigem: ");
 				scanf("%d", &origem);
@@ -146,32 +176,36 @@ void menu(lista **g){
                 arestas--;
                 limpa_tela();
 				break;
-			
+
 			case 3:
 				limpa_tela();
-				ImprimirGrafo(g,n);
+				ImprimirGrafo(g, n);
 				printf("\n");
 				break;
-			
+
 			case 4:
+        printf("Insira o valor do vertice: ");
+        scanf("%d", &origem);
+        limpa_tela();
+        entradaVertice(g, origem, n);
+        saidaVertice(g[origem], 0);
 				break;
-			
+
 			case 5:
 				break;
-			
+
 		}
 	}
-	
+
 }
 
 int main(){
-	
+
 	lista **g=NULL;
 	menu(g);
-	
+
 	return 0;
 }
-
 
 
 
