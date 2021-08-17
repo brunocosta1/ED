692694af81f6e3a8789a71832433449d10724263
brunocosta1 <brunocosta.cg1998@gmail.com>
2021-08-17 11:47:38 -0300

    Adicionando mais uma forma de compilar o programa

--- TAD/main.c
+++ TAD/main.c
@@ -1,6 +1,10 @@
 // Alunos: Bruno Costa dos Passos, Raphael Yoshiki, Kamile Novaes
 
 // Para compilar o programa com o gcc, basta usar o comando: gcc main.c fila.c -o main -lm
+// Ou então com os seguintes comandos:
+// gcc -c main.c
+// gcc -c fila.c
+// gcc -o main main.o fila.o
 
 #include "fila.h"
 #include <stdio.h>
