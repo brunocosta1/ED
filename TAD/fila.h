3ccbf3783cbd91e48be629ac57791f961cfb60f3
brunocosta1 <brunocosta.cg1998@gmail.com>
2021-08-17 11:38:05 -0300

    Realocando a função Menu()

--- TAD/fila.h
+++ TAD/fila.h
@@ -8,7 +8,6 @@ Fila Destruir(Fila f);
 void Inserir(Fila f, int x);
 void Remove(Fila f);
 void Imprimir(Fila f);
-void Menu();
 
 
 #endif
--- TAD/main.c
+++ TAD/main.c
@@ -3,6 +3,52 @@
 // Para compilar o programa com o gcc, basta usar o comando: gcc main.c fila.c -o main -lm
 
 #include "fila.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// Implementação do menu do outro programador usando a fila.h
+
+
+void Menu(){
+
+    int opc = -1, temp;
+    Fila fila = Inicializar();
+
+    while(opc != 4){
+
+        printf("Opções:\n");
+
+        printf("[0] - Inserir elemento na fila.\n");
+        printf("[1] - Remover elemento da fila.\n");
+        printf("[2] - Imprimir fila.\n");
+        printf("[3] - Sair.\n");
+
+        scanf("%d", &opc);
+
+
+        switch(opc){
+
+            case 0:
+
+                printf("Digite o elemento a ser inserido:\n");
+                scanf("%d", &temp);
+                Inserir(fila, temp);
+                system("clear");
+                break;
+            
+            case 1:
+                Remove(fila);
+                break;
+
+            case 2:
+                Imprimir(fila);
+                break;
+            case 3:
+                exit(0);
+                break;
+        }
+    }
+}
 
 int main(){
 
